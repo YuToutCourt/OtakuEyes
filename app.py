c8ff3fb95cff8c7588e865f585603637f043e72f
maleyy <wapzif@gmail.com>
2023-05-11 10:04:44 +0200

    update lib, and new link to watch

--- app.py
+++ app.py
@@ -75,4 +75,4 @@ def top_anime():
                             this_season=this_season, current_season=current_season)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
