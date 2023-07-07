bd9ae9df2648fdbb0bf4af1b4f0379a11f02c827
maleyy <wapzif@gmail.com>
2023-07-07 15:37:39 +0200

    make the text appers only if the localstorage have data

--- static/js/save.js
+++ static/js/save.js
@@ -53,5 +53,45 @@ function storeAnimeAndEpisode(episode, id, image, title) {
         localStorage.setItem('anime', JSON.stringify(animeArray));
     }
 }
+function loadLocalStorage() {
+    var storedAnime = localStorage.getItem('anime');
 
-// function
+    if (storedAnime) {
+        // Parse the stored anime data from JSON to an array of objects
+        var storedAnimeArray = JSON.parse(storedAnime);
+
+        // Get the anime-list element
+        var animeList = document.getElementById('anime-list');
+        var h3 = document.createElement('h3');
+        h3.appendChild(document.createTextNode('Continue watching'));
+        document.getElementsByClassName('container')[0].insertBefore(h3, document.getElementById('anime-list'));
+
+        // Loop through the stored anime data and create HTML elements for each entry
+        storedAnimeArray.forEach(function(anime_) {
+            
+            // Create HTML elements for anime entry
+            var article = document.createElement('article');
+
+            var linkImage = document.createElement('a');
+            linkImage.setAttribute('href', `anime/${anime_['id']}/${anime_['episode']}/vostfr`);
+
+            var image = document.createElement('img');
+            image.setAttribute('src', anime_['image']);
+            image.setAttribute('class', 'coverimage');
+
+            var linkTitle = document.createElement('a');
+            linkTitle.setAttribute('href', `anime/${anime_['id']}/${anime_['episode']}/vostfr`);
+            linkTitle.setAttribute('class', 'title');
+            var titleText = document.createTextNode(`${anime_['title']} - Ep ${anime_['episode']}`);
+            linkTitle.appendChild(titleText);
+
+            // Append elements to the article
+            linkImage.appendChild(image);
+            article.appendChild(linkImage);
+            article.appendChild(linkTitle);
+
+            // Append the article to the anime-list element
+            animeList.appendChild(article);
+        });
+    }
+};
